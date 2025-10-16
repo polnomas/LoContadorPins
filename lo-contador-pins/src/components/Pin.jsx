28ba9e8aaed1ac1444f3acf7dec1e8c69ea8107d
polnomas <112011287+polnomas@users.noreply.github.com>
2025-10-16 17:21:36 -0300

    Fix SVG sizing and interactions for campus map

--- lo-contador-pins/src/components/Pin.jsx
+++ lo-contador-pins/src/components/Pin.jsx
@@ -1,12 +1,13 @@
 import '../styles/Pin.css'
-//NOTE: id podría agregarse como otro atributo al final
-function Pin({ x, y }) { 
+
+// NOTE: id podría agregarse como otro atributo al final
+function Pin({ x, y }) {
     return (
-        <div 
-            className="pin" 
+        <div
+            className="pin"
             style={{
                 left: `${x}px`,
-                top: `${y}px`
+                top: `${y}px`,
             }}
         />
     )
