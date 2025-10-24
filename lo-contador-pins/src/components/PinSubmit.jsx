33ba74d806b7f6571129fb5ab58e4c568918e482
Pablo Altamirano <p.j.altamirano.p@gmail.com>
2025-10-24 02:55:04 -0300

    text forms

--- lo-contador-pins/src/components/PinSubmit.jsx
+++ lo-contador-pins/src/components/PinSubmit.jsx
@@ -4,10 +4,24 @@ import PinImage from "./PinImage"
 
 function PinSubmit() {
     const [publicId, setPublicId] = useState(null)
+    const [description, setDescription] = useState("")
+
+    const handleDescriptionChange = (e) => {
+        setDescription(e.target.value)
+    }
+
     return (
         <div>
             {publicId ? <PinImage publicId={publicId} /> : null}
             <UploadWidget publicIdSetter={setPublicId}/>
+            <textarea
+                id="desc"
+                value={description}
+                onChange={handleDescriptionChange}
+                rows={3}
+                placeholder="Escribe una descripción…"
+                style={{ width: "100%", maxWidth: "400px" }}
+            />
         </div>
     )
 }
